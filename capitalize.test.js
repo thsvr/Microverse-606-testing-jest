299bdb4079f4f3f10165243107700f738f279d63
Thaís Vieira <vr.ths.zd@gmail.com>
2020-03-03 13:47:45 -0300

    Add tests for capitalize function.

---
+++ capitalize.test.js
@@ -0,0 +1,14 @@
+const capitalize = require('./capitalize');
+
+
+it('returns a string', () => {
+  expect(typeof capitalize('thais')).toBe('string');
+});
+
+it('returns a capitalized string (1)', () => {
+  expect(capitalize('thais')).toBe('Thais');
+});
+
+it('returns a capitalized string (2)', () => {
+  expect(capitalize('sarah')).toBe('Sarah');
+});
